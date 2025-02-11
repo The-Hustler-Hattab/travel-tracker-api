d8b6daa6d045553687696a2cb5b9a9ca462337e1
Mohammed Hattab <mohammedhattab97@gmail.com>
2025-02-10 20:09:54 -0500

    updated table

--- src/db/models/users-model.go
+++ src/db/models/users-model.go
@@ -2,7 +2,7 @@ package models
 
 type User struct {
 	ID       int    `gorm:"primaryKey;autoIncrement"`
-	Email    string `gorm:"size:200;unique;not null"`
+	Email    string `gorm:"size:200;not null"`
 	Password string `gorm:"size:200;not null"`
 }
 
